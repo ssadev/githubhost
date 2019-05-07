1cf6ebebecaf036bda6501ed2c919d204d0fd550
programworld999 <42247997+programworld999@users.noreply.github.com>
2019-05-07 17:47:03 +0530

    Update script.js

--- test/script.js
+++ test/script.js
@@ -1,5 +1,5 @@
 document.write(
-  '<link rel="stylesheet" href="https://github.githubassets.com/assets/gist-embed-a9a1cf2ca01efd362bfa52312712ae94.css">'
+  '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">'
 );
 document.write(
   '<div class="card"><h5 class="card-header">Featured</h5><div class="card-body"><h5 class="card-title">Special title treatment</h5><p class="card-text">With supporting text below as a natural lead-in to additional content.</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div>'
